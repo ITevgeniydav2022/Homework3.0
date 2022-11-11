7610aee1a8abfe07af36e62e840bb67735a1583f
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-12 00:52:26 +0300

    Сделано ДЗ Условные оператор ч.1 Задание 7,8,9

--- src/Main.java
+++ src/Main.java
@@ -66,15 +66,63 @@ public class Main {
         int standingUsed1 = 42;
         if (allSeatsUsed1 < allSeats1) {
             System.out.println("Есть еще " + (allSeats1 - allSeatsUsed1) + " сидячих мест ");
-        } else if (allSeatsUsed1 == allSeats1) {
+        } else {
             System.out.println("Сидячих мест нет!");
         }
         if (standingUsed1 < standingSeats1) {
             System.out.println("Есть ещё " + (standingSeats1 - standingUsed1) + " стоячих мест ");
-        } else if (standingUsed1 == standingSeats1) {
+        } else {
             System.out.println("Стоячих мест нет!");
         }
+        // Задание 7
+        System.out.println("Задание 7");
+        int age2 = 19;
+        if (age2 >= 2 && age2  <= 6) {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в детский сад ");
+        } else if (age2 > 6 && age2 <= 18) {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в школу ");
+        } else if (age2 > 18 && age2 <= 24) {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в университет");
+        } else if (age2 > 24) {
+            System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить на работу ");
+        }
+        // Задание 8
+        System.out.println("Задание 8");
+        int age3 = 15;
+        if (age3 < 5) {
+            System.out.println(" Ребёнок не может кататься на аттракционе ");
+        } else if (age3 < 14) {
+            System.out.println(" Ребенок может кататься на аттракционе, только в сопровождении взрослого ");
+        } else {
+            System.out.println(" Ребенок может кататься на аттракционе ");
+        }
 
+        // Задание 9
+        System.out.println("Задание 9");
+        int one = 10;
+        int two = 10;
+        int three = 10;
+        if (one > two) {
+            if (one >= three) {
+                System.out.println("Максимальное число - " + one);
+            } else {
+                System.out.println("Максимальное число - " + three);
+            }
+        } else if (two > one) {
+            if (two >= three) {
+                System.out.println("Максимальное число - " + two);
+            } else {
+                System.out.println("Максимальное число - " + three);
+            }
+        } else {
+            if (one > three) {
+                System.out.println("Максимальное число - " + one);
+            } else if (three > one) {
+                System.out.println("Максимальное число - " + three);
+            } else {
+                System.out.println("Все три числа равны");
+            }
+        }
     }
 }
 
