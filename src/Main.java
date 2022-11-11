57fc2c455e23a28a0042835a757c0dfb1b1d9713
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-11 23:18:51 +0300

    Сделано ДЗ Условные оператор ч.1 Задание 4,5,6

--- src/Main.java
+++ src/Main.java
@@ -40,6 +40,41 @@ public class Main {
         if (standingUsed == standingSeats) {
             System.out.println("Стоячих мест нет!");
         }
+        // Задание 4
+        System.out.println("Задание 4");
+        int age1 = 17;
+        if (age1 >= 18) {
+            System.out.println("Поздравляем! Вы достигли совершеннолетия!");
+        } else {
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно чутка подождать!");
+        }
+        // Задание 5
+        System.out.println("Задание 5");
+        if (age >= 7 && age < 18) {
+            System.out.println("Ребенок ходит в школу");
+        } else if (age >= 18 && age < 24) {
+            System.out.println("Человек уже закончил школу и ходит в универ");
+        } else if (age >= 24) {
+            System.out.println("Человек окончил университет и ходит на работу");
+        }
+        // Задание 6
+        System.out.println("Задание 6");
+        int capacity1 = 102;
+        int allSeats1 = 60;
+        int standingSeats1 = capacity1 - allSeats1;
+        int allSeatsUsed1 = 60;
+        int standingUsed1 = 42;
+        if (allSeatsUsed1 < allSeats1) {
+            System.out.println("Есть еще " + (allSeats1 - allSeatsUsed1) + " сидячих мест ");
+        } else if (allSeatsUsed1 == allSeats1) {
+            System.out.println("Сидячих мест нет!");
+        }
+        if (standingUsed1 < standingSeats1) {
+            System.out.println("Есть ещё " + (standingSeats1 - standingUsed1) + " стоячих мест ");
+        } else if (standingUsed1 == standingSeats1) {
+            System.out.println("Стоячих мест нет!");
+        }
+
     }
 }
 
