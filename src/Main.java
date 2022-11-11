f7548c8329171e55ce6c52307916031f422de20c
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-11 20:50:35 +0300

    Сделано ДЗ Условные оператор ч.1 Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+        int age = 21;
+        if (age >= 18) {
+            System.out.println("Поздравляем! Вы достигли совершеннолетия!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил, нужно чутка подождать!");
+        }
     }
 }
+
